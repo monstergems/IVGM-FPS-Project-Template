c1b0cb5dfaac04c7aaf2d181c87bbff612ffd025
monstergems <90658459+monstergems@users.noreply.github.com>
2022-10-28 12:53:37 +0200

    health pack change and helpfull text

---
+++ Assets/02_Student Folders/aaron's stuff/Scripts/notification.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class notification : MonoBehaviour
+{
+    public GameObject textDisplay;
+    public string text;
+    void Start()
+ {
+    
+    
+ }
+ 
+ void Update () 
+ {
+
+ }
+
+}
