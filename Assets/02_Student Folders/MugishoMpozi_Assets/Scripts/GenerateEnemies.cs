4cc3a1819a4211da342bf279674989801960cbeb
Mugisho <mugishompozi@gmail.com>
2022-10-29 02:26:38 +0200

    refactor: script without gameobject.find

--- Assets/02_Student Folders/MugishoMpozi_Assets/Scripts/GenerateEnemies.cs
+++ Assets/02_Student Folders/MugishoMpozi_Assets/Scripts/GenerateEnemies.cs
@@ -6,18 +6,11 @@ public class GenerateEnemies : MonoBehaviour
 {
 
     public GameObject enemy;
-    public GameObject player = GameObject.Find("Player");
+    public GameObject player;
     public int xPos;
     public int yPos;
-
-    public int roomMinXPos;
-
-    public int roomMinZPos;
-    public int roomMaxXPos;
-    public int roomMaxZPos;
     public int enemyCount;
-
-    private float spawnIntervalShort = 3.5f;
+    public float spawnIntervalShort = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -26,16 +19,17 @@ public class GenerateEnemies : MonoBehaviour
     }
 
     IEnumerator spawnEnemy(float spawnIntervalShort) {
-        var playerPostionX = GameObject.Find("Player").transform.position.x;
-        var playerPostionY = GameObject.Find("Player").transform.position.y;
-        if (playerPostionX > roomMinXPos && playerPostionX < roomMaxXPos &&
-        playerPostionY > roomMinYPos && playerPostionY < roomMaxYPos) {
-            xPos = Random.Range(roomMinXPos, roomMaxXPos);
-            yPos = Random.Range(roomMinYPos, roomMaxYPos);
+        var playerPostionX = player.transform.position.x;
+        var playerPostionY = player.transform.position.y;
+        if (playerPostionX > -17 && playerPostionX < 21 &&
+        playerPostionY > -5 && playerPostionY < 22) {
+            xPos = Random.Range(-17, 21);
+            yPos = Random.Range(-5, 22);
             Instantiate(enemy, new Vector3(xPos, 1, yPos), Quaternion.identity);
             yield return new WaitForSeconds(spawnIntervalShort);
         }
 
+        StartCoroutine(spawnEnemy(spawnIntervalShort));
     }
 
     // // Update is called once per frame
