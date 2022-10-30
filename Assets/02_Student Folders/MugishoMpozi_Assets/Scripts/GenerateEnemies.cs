b2e9f1edde97c60623bf8767c92c6c9f651fff50
Mugisho <mugishompozi@gmail.com>
2022-10-30 19:43:15 +0100

    implement basic crystal functionality

--- Assets/02_Student Folders/MugishoMpozi_Assets/Scripts/GenerateEnemies.cs
+++ Assets/02_Student Folders/MugishoMpozi_Assets/Scripts/GenerateEnemies.cs
@@ -6,35 +6,47 @@ public class GenerateEnemies : MonoBehaviour
 {
 
     public GameObject enemy;
-    public GameObject player;
     public int xPos;
     public int yPos;
-    public int enemyCount;
+    public int maxNumEnemies;
+    public int killedEnemies;
     public float spawnIntervalShort = 2f;
+    public List<GameObject> spawnedEnemies = new List<GameObject>();
+
+    
+    private GameObject player;
+    private Vector3 playerPos;
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player");
         StartCoroutine(spawnEnemy(spawnIntervalShort));
     }
 
     IEnumerator spawnEnemy(float spawnIntervalShort) {
-        var playerPostionX = player.transform.position.x;
-        var playerPostionY = player.transform.position.y;
-        if (playerPostionX > -17 && playerPostionX < 21 &&
-        playerPostionY > -5 && playerPostionY < 22) {
-            xPos = Random.Range(-17, 21);
-            yPos = Random.Range(-5, 22);
-            Instantiate(enemy, new Vector3(xPos, 1, yPos), Quaternion.identity);
-            yield return new WaitForSeconds(spawnIntervalShort);
+        if ((playerPos.x > -17 && playerPos.x < 21) &&
+        (playerPos.z > -5 && playerPos.z < 22)) {
+            if (spawnedEnemies.Count < maxNumEnemies) {
+                xPos = Random.Range(-17, 21);
+                yPos = Random.Range(-5, 22);
+                spawnedEnemies.Add(Instantiate(enemy, new Vector3(xPos, 1, yPos), Quaternion.identity));
+            }
         }
 
+        yield return new WaitForSeconds(spawnIntervalShort);
         StartCoroutine(spawnEnemy(spawnIntervalShort));
     }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = player.transform.position;
+        for (int i = 0; i < spawnedEnemies.Count; i++) {
+            if (spawnedEnemies[i] == null) {
+                spawnedEnemies.RemoveAt(i);
+                killedEnemies++;
+            }
+        }
+    }
 }
